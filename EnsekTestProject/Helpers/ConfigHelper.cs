a3b0738349ba4a0dc881fe1bb92b219a87aa1dfc
suryas <surya.g13@gmail.com>
2023-07-11 23:40:44 +0100

    Added config values

---
+++ EnsekTestProject/Helpers/ConfigHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EnsekTestProject.Helpers
+{
+    public static class ConfigHelper
+    {
+        public static IConfigurationRoot GetConfiguration ()
+        {
+            var configBuilder = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                                .AddJsonFile("appsettings.json", true).Build();
+
+            return configBuilder;
+        
+        }
+
+    }
+}
+
